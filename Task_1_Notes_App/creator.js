82015f22041f3715f4cea1382a4de26ab7834c22
Valiantsin Baliakin <valentinakaryuk@gmail.com>
2020-03-06 17:24:57 +0300

    update Task1 - Note App, Task2 - File Watcher

--- Task_1_Notes_App/creator.js
+++ Task_1_Notes_App/creator.js
@@ -1,19 +1,17 @@
-const fs = require("fs");
-const path = require('path');
-const pathToNotesStorage = path.join('./notes.json');
+const fs = require("fs"),
+    path = require("path"),
+    pathToNotesStorage = path.join("./notes.json");
 
 const getAllNotes = () => {
     return JSON.parse(fs.readFileSync(pathToNotesStorage));
 };
 
 const createNote = (title, body) => {
-    const note = {};
+    const note = {},
+        array = getAllNotes();
     note.title = title;
     note.body = body;
-    const array = getAllNotes();
-    // console.log(array); // проверка работоспособности
     const dublicateFinder = array.find(element => element.title === title);
-    // console.log(dublicateFinder); // проверка результата работы команды
     if (dublicateFinder) {
         console.log("You create dublicate of note!");
     } else {
@@ -34,19 +32,15 @@ const listOfNotes = () => {
 };
 
 const readNote = (title) => {
-    const array = getAllNotes();
-    const noteFinder = array.find(element => element.title === title);
-    if (noteFinder) {
-        console.log(`Your note:\n${title} - ` + noteFinder.body);
-    } else {
-        console.log("Note isn't exist!");
-    }
+    const array = getAllNotes(),
+        noteFinder = array.find(element => element.title === title);
+    result = noteFinder ? `Your note:\n${title} - ` + noteFinder.body : "Note isn't exist!";
+    console.log(result);
 };
 
 const removeNote = (title) => {
     const array = getAllNotes();
     const sortedNotes = array.length !== 0 ? array.filter(note => note.title !== title) : [];
-    // удаляем (фильтруем) заметку по заданному title
     if (array.length !== sortedNotes.length) {
         console.log(`Note with title -  ${title} - succesfully deleted!`);
         fs.writeFileSync(pathToNotesStorage, JSON.stringify(sortedNotes, null, '\t'));
@@ -56,7 +50,7 @@ const removeNote = (title) => {
     }
 };
 
-module.exports = { // экспорт созданных функций
+module.exports = {
     createNote,
     listOfNotes,
     readNote,
--- Task_1_Notes_App/note.js
+++ Task_1_Notes_App/note.js
@@ -1,53 +1,46 @@
-const yargs = require('yargs');
-const packageJSON = require('./package.json');
-yargs.version(packageJSON.version);
-const creator = require('./creator');
+const yargs = require("yargs"),
+    creator = require("./creator");
 
-yargs.command({ // АПГРЕЙД ДО ИСЧЕЗНОВЕНИЯ --body (глянуть yargs)
-    command: 'add',
-    describe: 'add new unique note',
+yargs.command({
+    command: "add",
+    describe: "add new unique note",
     builder: {
-        title: { // название создаваемой заметки
-            type: 'string', // тип создаваемй заметки
-            demandOption: true, // этот параметр (title) обязателен
+        title: {
+            type: "string",
+            demandOption: true,
             alias: "t",
-            describe: 'Title of new note'
+            describe: "Title of new note"
         },
-        body: { // текст внутри заметки
-            type: 'string',
+        body: {
+            type: "string",
             demandOption: true,
             alias: "b",
-            describe: 'Content of new note'
+            describe: "Content of new note"
         }
     },
     handler({
         title,
         body
-    }) { // hadler - ссылка на функцию-обработчик
-        // console.log('ADD command: ', {title, body});
-        // в итоге, передавая два аргумента внутрь hadler мы получаем в консоль
-        // ...значения title и body в порядке их вызова
+    }) { 
         creator.createNote(title, body);
-        // после экспорта из creator.js мы здесь вызываем функцию по созданию
-        // ... заметки со своим title и body
     }
 });
 yargs.command({
-    command: 'list',
-    describe: 'list all notes',
+    command: "list",
+    describe: "list all notes",
     handler() {
         creator.listOfNotes();
     }
 });
 yargs.command({
-    command: 'read',
-    describe: 'read one note by its title',
+    command: "read",
+    describe: "read one note by its title",
     builder: {
         title: {
-            type: 'string',
+            type: "string",
             demandOption: true,
             alias: "t",
-            describe: 'Title of new note'
+            describe: "Title of new note"
         }
     },
     handler({
@@ -57,14 +50,14 @@ yargs.command({
     }
 });
 yargs.command({
-    command: 'remove',
-    describe: 'remove one note by its title',
+    command: "remove",
+    describe: "remove one note by its title",
     builder: {
         title: {
-            type: 'string',
+            type: "string",
             demandOption: true,
             alias: "t",
-            describe: 'Title of new note'
+            describe: "Title of new note"
         }
     },
     handler({
@@ -74,4 +67,4 @@ yargs.command({
     }
 });
 
-yargs.parse(); // читает из консоли данные
+yargs.parse();
